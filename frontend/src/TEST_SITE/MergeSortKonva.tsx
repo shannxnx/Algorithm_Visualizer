fdb524fde0ac87c339e6c69a2b469f53318d7d95
shann <shannrhey123@gmail.com>
2025-08-18 02:47:25 +0800

    WIP

--- frontend/src/TEST_SITE/MergeSortKonva.tsx
+++ frontend/src/TEST_SITE/MergeSortKonva.tsx
@@ -36,6 +36,10 @@ export const MergeSortKonva: React.FC<KonvaProps> = ({ x, y, boxesInfo }) => {
 
     const [rectArray, setRectArray] = useState<Array<rectInfo>>([]);
 
+    //test
+    const [left, setLeft] = useState<Array<rectInfo>>([]);
+    const [right, setRight] = useState<Array<rectInfo>>([]);
+
     const stageHeight = y + 50 + 50;
     console.log("X in mergeKonva: ", x);
 
@@ -43,27 +47,65 @@ export const MergeSortKonva: React.FC<KonvaProps> = ({ x, y, boxesInfo }) => {
     function generateArray(length: number) {
         const array: Array<rectInfo> = [];
 
+
+
+        const rectWidth = length > 6 ? 35 : 50;
+        const spacing = 5;
+        const totalWidth = length * rectWidth + (length - 1) * spacing
+
+        const startX = (konvaWidth / 2) - (totalWidth / 2);
+
+
         for (let i = 0; i < length; i++) {
+
             const rectangle: rectInfo = {
-                x: (i * 55) + (konvaWidth / 2),
+                x: startX + i * (rectWidth + spacing),
                 y: -45,
-                width: 50,
-                height: 50,
+                width: rectWidth,
+                height: rectWidth,
                 id: i,
-                number: i + 1
+                number: Math.floor(Math.random() * 100)
             }
             array.push(rectangle);
         }
         setRectArray(array);
 
+    };
+
+    function splitArray(array: Array<rectInfo>) {
+        if (array) {
+            const arrayLength = array?.length;
+            const leftLength = Math.floor(arrayLength / 2);
+            const rightLength = arrayLength - leftLength;
+
+            const leftArray: Array<rectInfo> = [];
+            const rightArray: Array<rectInfo> = [];
+
+            for (let i = 0; i < leftLength; i++) {
+                const rect: rectInfo = array[i];
+
+                leftArray.push(rect);
+            };
+
+            for (let i = rightLength - 1; i < arrayLength; i++) {
+                const rect: rectInfo = array[i];
+
+                rightArray.push(rect);
+            };
+
+            setLeft(leftArray);
+            setRight(rightArray);
+        }
     }
 
     useEffect(() => {
-        generateArray(5);
+        generateArray(7);
+
 
     }, []);
 
-    console.log("Array merge: ", rectArray);
+
+
 
 
 
@@ -110,6 +152,49 @@ export const MergeSortKonva: React.FC<KonvaProps> = ({ x, y, boxesInfo }) => {
 
 
                 ))}
+
+                {
+                    //rectArray.map((r, id) => (
+                    //    <Group
+                    //        draggable
+                    //        key={`group-${id}`}
+                    //        x={r.x}
+                    //        y={r.y + 120} // Start 50px lower to accommodate lift
+                    //    >
+                    //        <Rect
+                    //            width={r.width}
+                    //            height={r.height}
+                    //            fill={r.color || "red"}
+                    //            draggable
+                    //        />
+                    //        <Text
+                    //            text={`${r.number}`}
+                    //            width={r.width}
+                    //            height={r.height}
+                    //            align="center"
+                    //            verticalAlign="middle"
+                    //            fill="white"
+                    //            fontSize={20}
+                    //        />
+
+                    //        {/*Small bug in here fix this in the near future*/}
+                    //        {
+                    //            //<Text
+                    //            //    text={`${id}`}
+                    //            //    y={r.height + 5}
+                    //            //    width={r.width}
+                    //            //    height={20}
+                    //            //    align="center"
+                    //            //    verticalAlign="top"
+                    //            //    fill="black"
+                    //            //    fontSize={12}
+                    //            ///>
+                    //        }
+                    //    </Group>
+
+
+                    //))
+                }
 
 
 
