0ba5473f5b4e1c46a0b7baba9e4f884b720bbf85
shann <shannrhey123@gmail.com>
2025-08-31 23:39:51 +0800

    responsive buttons design v1

--- frontend/src/ALGORITHMS/SORT/QUICK_SORT/QuickSort.tsx
+++ frontend/src/ALGORITHMS/SORT/QUICK_SORT/QuickSort.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import AlgoInfo from "../../../COMPONENTS/INFO_CONTENT/AlgoInfo";
 import { sortStore } from "../../../STATE/sortingStore";
 import type { SortKit } from "../../../INTERFACES/sortInterface";
+import { ArrowLeft, BetweenVerticalEnd, BetweenVerticalStart, Delete, Play, Plus, Scissors, Shuffle } from "lucide-react";
 
 
 export default function QuickSort() {
@@ -38,106 +39,117 @@ export default function QuickSort() {
             <div className="w-[95%] lg:h-[75%] h-[85%] border-1
              flex items-center justify-center rounded-[8px] duration-200 bg-white/70 backdrop-blur-sm shadow-xl m-4 
              overflow-x-scroll border-black">
-
+                NIGGA
             </div>
 
 
 
-            <div className="w-[95%] lg:h-[120px] h-[15%] flex items-center lg:justify-around justify-center rounded mb-3 border-black border">
+            <div className="w-[95%] lg:h-[120px] h-[15%] flex items-center  rounded mb-3
+             border-black justify-start border">
 
-                <div className="w-[35%] h-full border-1 flex flex-col border-black">
+                <div className="lg:w-[35%] w-[35%]  h-full">
 
-                    <div className="w-full h-1/2 flex justify-around items-center border-b-1 border-black">
+                    {/*Add Pop New (buttons)*/}
+                    <div className="w-full h-1/2 border-r flex justify-center items-center">
 
-                        <button className="lg:text-2xl border-1  lg:h-[36px] lg:w-[64px] disabled:opacity-50 lg:rounded
-                        cursor-pointer hover:scale-105 duration-150 text-black"
 
-                        >
-                            Add
-                        </button>
+                        <div className="w-[90%] gap-1 h-[70%] flex justify-center items-center">
 
-                        <button className="lg:text-2xl border-1  lg:h-[36px] lg:w-[64px] disabled:opacity-50 lg:rounded
-                        cursor-pointer hover:scale-105 duration-150 text-black"
+                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 
+                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
 
-                        >
-                            Pop
-                        </button>
+                                <Plus className="" />
+                                Add
 
-                        <button className="lg:text-2xl border-1  lg:h-[36px] lg:w-[64px] disabled:opacity-50 lg:rounded
-                        cursor-pointer hover:scale-105 duration-150 text-black"
+                            </button>
 
-                        >
-                            New
-                        </button>
+                            <button className="w-[33%] border lg:h-full h-[80%]  rounded cursor-pointer hover:scale-105 duration-150 
+                            active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+
+                                <Delete className="" />
+                                Pop
+
+                            </button>
+
+                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150
+                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+
+                                <Shuffle />
+                                New
+
+                            </button>
+                        </div>
+
+
 
                     </div>
 
-
-                    <div className="w-full h-[50%] flex items-center justify-center">
-                        <button className="lg:text-3xl border-1 lg:w-[130px] rounded bg-green-400
-                        disabled:opacity-50 cursor-pointer hover:scale-105 duration-150 text-black"
-                        >
+                    {/*Animate button*/}
+                    <div className="w-full h-1/2 border-t border-r flex items-center justify-center">
+                        <button className="border text-[18px] lg:text-3xl p-1 rounded cursor-pointer hover:scale-105 duration-150
+                    active:scale-100 flex items-center ">
+                            <Play />
                             Animate
                         </button>
+
+                    </div>
+
+
+                </div>
+
+
+
+                {/*Insert and Remove buttons*/}
+                <div className="lg:w-[60%] w-[55%]  h-full flex flex-col items-center">
+
+                    <div className="w-full h-1/2 flex p-1 items-center ">
+
+                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
+                        duration-150 lg:text-[24px] flex items-center justify-center">
+                            <BetweenVerticalStart />
+                            Insert
+                        </button>
+
+
+                        <label htmlFor="value" className="w-[12%] overflow-x-hidden lg:ml-2">Value</label>
+                        <input type="number" className="w-[22%] ml-1 border-b"
+                            name="value" />
+
+                        <label htmlFor="index" className="w-[12%] overflow-x-hidden lg:ml-2">
+                            Index
+                        </label>
+
+                        <input type="number" className="w-[22%] ml-1 border-b"
+                            name="index" min={0} />
+
+
+                    </div>
+
+                    <div className="w-full h-1/2 border-t flex p-1 items-center">
+
+                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
+                        duration-150 lg:text-[24px] flex items-center justify-center">
+                            <Scissors />
+                            Remove
+                        </button>
+
+
+                        <label htmlFor="index" className="ml-2">index</label>
+                        <input type="number" className="w-[25%] ml-1 border-b"
+                            name="index" min={0} />
+
+
+
                     </div>
                 </div>
 
 
-                <div className="w-[60%] h-full  border-black
-                        flex items-center justify-end cursor-pointer gap-2 bg-white">
 
-                    <div className="w-[90%] h-full border-1 flex flex-col items-center
-                            justify-center gap-2 border-black">
-
-                        <div className="w-[80%] h-[40%] flex items-center">
-
-                            <button className="lg:text-2xl lg:h-full border-1
-                             p-1 rounded hover:scale-105 duration-100 cursor-pointer
-                            bg-green-400 disabled:opacity-50 text-black "
-
-                            >
-                                Insert
-                            </button>
-                            <label htmlFor="value" className="ml-1 text-black">Value</label>
-
-                            <input type="number" name="value" className="border-1 lg:w-[54px] ml-3 text-[16px] p-1
-                            outline-none text-black w-[20px]"
-                            />
-
-
-                            <label htmlFor="index" className="ml-1 text-black">Index</label>
-                            <input type="number" name="index"
-                                className="border-1 lg:w-[54px] lg:text-[16px] ml-2 p-1
-                            outline-none text-black w-[20px] "
-
-                            />
-
-
-                        </div>
-
-                        <div className="w-[80%] h-[40%]  items-center flex">
-                            <button className="lg:text-2xl lg:h-full border-1 p-1 
-                            rounded hover:scale-105 duration-100 cursor-pointer
-                            bg-red-500 disabled:opacity-50 text-black"
-
-                            >
-                                Remove
-                            </button>
-                            <label htmlFor="indexR" className="ml-1 text-black">Index</label>
-
-                            <input type="number" className="border-1 w-[54px] ml-3 text-[16px] p-1
-                                    outline-none text-black"
-
-                            />
-
-                        </div>
-                    </div>
-
-
+                <div className="lg:w-[5%] w-[10%] border-l h-full flex items-center justify-center cursor-pointer">
+                    <ArrowLeft />
                 </div>
 
             </div>
-
 
 
 
