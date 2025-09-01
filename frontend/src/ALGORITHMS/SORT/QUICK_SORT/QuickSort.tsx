740a5354c3ad516a678ad70c18d819b14c6a077e
shann <shannrhey123@gmail.com>
2025-09-01 21:36:20 +0800

    buttons responsive done! (gonna improve in the future if new idea comes up)

--- frontend/src/ALGORITHMS/SORT/QUICK_SORT/QuickSort.tsx
+++ frontend/src/ALGORITHMS/SORT/QUICK_SORT/QuickSort.tsx
@@ -45,116 +45,87 @@ export default function QuickSort() {
             </div>
 
 
-            <div className="w-[95%] lg:h-[120px] h-[15%] flex items-center  rounded mb-3
-             border-black justify-start border">
+            <div className="w-[95%] lg:h-[120px] h-[15%] flex items-center rounded mb-3 border-black border">
+                <div className="h-full w-full flex justify-end">
 
-                <div className="lg:w-[35%] w-[35%]  h-full">
+                    <AnimatePresence>
+                        {showButtons && (
+                            <motion.div
+                                key="toolbar"
+                                initial={{ x: 300, opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                exit={{ x: 500, opacity: 0 }}
+                                transition={{ duration: 0.2 }}
+                                className="flex h-full lg:w-[95%] w-[90%] justify-start "
+                            >
 
-                    {/*Add Pop New (buttons)*/}
-                    <div className="w-full h-1/2 border-r flex justify-center items-center">
+                                {/* Left Section (Add / Pop / New + Animate) */}
+                                <div className="lg:w-[40%] w-[45%] h-full">
+                                    {/* Add / Pop / New */}
+                                    <div className="w-full h-1/2 border-r flex justify-center items-center">
+                                        <div className="w-[90%] gap-1 h-[70%] flex justify-center items-center">
+                                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+                                                <Plus /> Add
+                                            </button>
 
+                                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+                                                <Delete /> Pop
+                                            </button>
 
-                        <div className="w-[90%] gap-1 h-[70%] flex justify-center items-center">
+                                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+                                                <Shuffle /> New
+                                            </button>
+                                        </div>
+                                    </div>
 
-                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 
-                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+                                    {/* Animate Button */}
+                                    <div className="w-full h-1/2 border-t border-r flex items-center justify-center">
+                                        <button className="border text-[18px] lg:text-3xl p-1 rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 flex items-center">
+                                            <Play /> Animate
+                                        </button>
+                                    </div>
+                                </div>
 
-                                <Plus className="" />
-                                Add
+                                {/* Right Section (Insert + Remove) */}
+                                <div className="lg:w-[60%] w-[55%] h-full flex flex-col items-center">
+                                    {/* Insert */}
+                                    <div className="w-full h-1/2 flex p-1 items-center">
+                                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105 duration-150 lg:text-[24px] flex items-center justify-center">
+                                            <BetweenVerticalStart /> Insert
+                                        </button>
+                                        <label htmlFor="value" className="w-[12%] overflow-x-hidden lg:ml-2">Value</label>
+                                        <input type="number" className="w-[22%] ml-1 border-b" name="value" />
+                                        <label htmlFor="index" className="w-[12%] overflow-x-hidden lg:ml-2">Index</label>
+                                        <input type="number" className="w-[22%] ml-1 border-b" name="index" min={0} />
+                                    </div>
 
-                            </button>
+                                    {/* Remove */}
+                                    <div className="w-full h-1/2 border-t flex p-1 items-center">
+                                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105 duration-150 lg:text-[24px] flex items-center justify-center">
+                                            <Scissors /> Remove
+                                        </button>
+                                        <label htmlFor="index" className="ml-2">index</label>
+                                        <input type="number" className="w-[25%] ml-1 border-b" name="index" min={0} />
+                                    </div>
+                                </div>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
 
-                            <button className="w-[33%] border lg:h-full h-[80%]  rounded cursor-pointer hover:scale-105 duration-150 
-                            active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
-
-                                <Delete className="" />
-                                Pop
-
-                            </button>
-
-                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150
-                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
-
-                                <Shuffle />
-                                New
-
-                            </button>
-                        </div>
-
-
-
-                    </div>
-
-                    {/*Animate button*/}
-                    <div className="w-full h-1/2 border-t border-r flex items-center justify-center">
-                        <button className="border text-[18px] lg:text-3xl p-1 rounded cursor-pointer hover:scale-105 duration-150
-                    active:scale-100 flex items-center ">
-                            <Play />
-                            Animate
-                        </button>
-
-                    </div>
-
-
-                </div>
-
-
-
-                {/*Insert and Remove buttons*/}
-                <div className="lg:w-[60%] w-[55%]  h-full flex flex-col items-center">
-
-                    <div className="w-full h-1/2 flex p-1 items-center ">
-
-                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
-                        duration-150 lg:text-[24px] flex items-center justify-center">
-                            <BetweenVerticalStart />
-                            Insert
-                        </button>
-
-
-                        <label htmlFor="value" className="w-[12%] overflow-x-hidden lg:ml-2">Value</label>
-                        <input type="number" className="w-[22%] ml-1 border-b"
-                            name="value" />
-
-                        <label htmlFor="index" className="w-[12%] overflow-x-hidden lg:ml-2">
-                            Index
-                        </label>
-
-                        <input type="number" className="w-[22%] ml-1 border-b"
-                            name="index" min={0} />
-
-
-                    </div>
-
-                    <div className="w-full h-1/2 border-t flex p-1 items-center">
-
-                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
-                        duration-150 lg:text-[24px] flex items-center justify-center">
-                            <Scissors />
-                            Remove
-                        </button>
-
-
-                        <label htmlFor="index" className="ml-2">index</label>
-                        <input type="number" className="w-[25%] ml-1 border-b"
-                            name="index" min={0} />
-
-
-
+                    {/* Toggle Arrow */}
+                    <div
+                        className="lg:w-[5%] w-[10%] border-l h-full flex items-center justify-center cursor-pointer"
+                        onClick={() => setShowButtons(!showButtons)}
+                    >
+                        {showButtons ? <ArrowRight /> : <ArrowLeft />}
                     </div>
                 </div>
-
-
-
-                <div className="lg:w-[5%] w-[10%] border-l h-full flex items-center justify-center cursor-pointer">
-                    <ArrowLeft />
-                </div>
-
             </div>
 
 
 
-        </div>
+
+        </div >
 
 
         <AlgoInfo algoInfo={QuickPayload} />
--- frontend/src/TEST_SITE/RollingButtons.tsx
+++ frontend/src/TEST_SITE/RollingButtons.tsx
@@ -6,135 +6,204 @@ export default function RollingButtons() {
     const [showButtons, setShowButtons] = useState(false);
 
     return (
-        <div className="w-[95%] lg:h-[120px] h-[15%] flex items-center rounded mb-3
-             border-black justify-end border">
-            <div className="flex h-full flex-grow justify-end">
-
+        <div className="w-[95%] lg:h-[120px] h-[15%] flex items-center rounded mb-3 border-black">
+            <div className="flex h-[15%] w-[95%] border justify-end">
                 <AnimatePresence>
                     {showButtons && (
                         <motion.div
                             key="toolbar"
-                            initial={{ x: 300, rotate: 0, opacity: 0 }}
-                            animate={{ x: 0, rotate: 0, opacity: 1 }}
-                            exit={{ x: 500, rotate: 0, opacity: 0 }}
-                            transition={{ duration: 0.4, }}
-                            className="flex h-full w-full "
+                            initial={{ x: 300, opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            exit={{ x: 500, opacity: 0 }}
+                            transition={{ duration: 0.2 }}
+                            className="flex h-full"
                         >
-                            <div className="lg:w-[35%] w-[45%] h-full ">
-
-                                {/*Add Pop New (buttons)*/}
-                                <div className="w-full h-1/2 border-r  flex justify-center items-center">
-
-
-                                    <div className="w-[90%] gap-1 h-[70%]  flex justify-center items-center">
-
-                                        <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 
-                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
-
-                                            <Plus className="" />
-                                            Add
-
+                            <div className="lg:w-[45%] w-[45%] h-full">
+                                {/* Add / Pop / New buttons */}
+                                <div className="w-full h-1/2 border-r flex justify-center items-center">
+                                    <div className="w-[90%] gap-1 h-[70%] flex justify-center items-center">
+                                        <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+                                            <Plus /> Add
                                         </button>
 
-                                        <button className="w-[33%] border lg:h-full h-[80%]  rounded cursor-pointer hover:scale-105 duration-150 
-                            active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
-
-                                            <Delete className="" />
-                                            Pop
-
+                                        <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+                                            <Delete /> Pop
                                         </button>
 
-                                        <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150
-                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
-
-                                            <Shuffle />
-                                            New
-
+                                        <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+                                            <Shuffle /> New
                                         </button>
                                     </div>
-
-
-
                                 </div>
 
-                                {/*Animate button*/}
+                                {/* Animate button */}
                                 <div className="w-full h-1/2 border-t border-r flex items-center justify-center">
-                                    <button className="border text-[18px] lg:text-3xl p-1 rounded cursor-pointer hover:scale-105 duration-150
-                    active:scale-100 flex items-center ">
-                                        <Play />
-                                        Animate
+                                    <button className="border text-[18px] lg:text-3xl p-1 rounded cursor-pointer hover:scale-105 duration-150 active:scale-100 flex items-center">
+                                        <Play /> Animate
                                     </button>
-
                                 </div>
-
-
                             </div>
 
-
-
-                            {/*Insert and Remove buttons*/}
-                            <div className="lg:w-[60%] w-[55%] border  h-full flex flex-col items-center">
-
-                                <div className="w-full h-1/2 flex p-1 items-center ">
-
-                                    <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
-                        duration-150 lg:text-[24px] flex items-center justify-center">
-                                        <BetweenVerticalStart />
-                                        Insert
+                            {/* Insert and Remove buttons */}
+                            <div className="lg:w-[60%] w-[55%] border h-full flex flex-col items-center">
+                                <div className="w-full h-1/2 flex p-1 items-center">
+                                    <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105 duration-150 lg:text-[24px] flex items-center justify-center">
+                                        <BetweenVerticalStart /> Insert
                                     </button>
 
-
-                                    <label htmlFor="value" className="w-[12%] overflow-x-hidden lg:ml-2">Value</label>
-                                    <input type="number" className="w-[22%] ml-1 border-b"
-                                        name="value" />
+                                    <label htmlFor="value" className="w-[12%] overflow-x-hidden lg:ml-2">
+                                        Value
+                                    </label>
+                                    <input type="number" className="w-[22%] ml-1 border-b" name="value" />
 
                                     <label htmlFor="index" className="w-[12%] overflow-x-hidden lg:ml-2">
                                         Index
                                     </label>
-
-                                    <input type="number" className="w-[22%] ml-1 border-b"
-                                        name="index" min={0} />
-
-
+                                    <input type="number" className="w-[22%] ml-1 border-b" name="index" min={0} />
                                 </div>
 
                                 <div className="w-full h-1/2 border-t flex p-1 items-center">
-
-                                    <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
-                        duration-150 lg:text-[24px] flex items-center justify-center">
-                                        <Scissors />
-                                        Remove
+                                    <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105 duration-150 lg:text-[24px] flex items-center justify-center">
+                                        <Scissors /> Remove
                                     </button>
 
-
-                                    <label htmlFor="index" className="ml-2">index</label>
-                                    <input type="number" className="w-[25%] ml-1 border-b"
-                                        name="index" min={0} />
-
-
-
+                                    <label htmlFor="index" className="ml-2">Index</label>
+                                    <input type="number" className="w-[25%] ml-1 border-b" name="index" min={0} />
                                 </div>
                             </div>
-                        </motion.div>)
-                    }
+                        </motion.div>
+                    )}
                 </AnimatePresence>
 
-
-                <div className="lg:w-[5%] w-[10%] border h-full flex items-center justify-center cursor-pointer"
-                    onClick={() => setShowButtons(!showButtons)}>
-                    {
-                        showButtons ? <ArrowRight className="size-[24px]" /> : <ArrowLeft className="size-[24px]" />
-                    }
-
+                {/* Arrow toggle button */}
+                <div
+                    className="lg:w-[5%] w-[10%] border h-[15%] flex items-center justify-center cursor-pointer"
+                    onClick={() => setShowButtons(!showButtons)}
+                >
+                    {showButtons ? <ArrowRight className="size-[24px]" /> : <ArrowLeft className="size-[24px]" />}
                 </div>
-
             </div>
+        </div>
 
-
-
-        </div >
     );
 }
 
+
+
+
+
+
+
+
+
+
+
+
+//<div className="w-[95%] lg:h-[120px] h-[15%] flex items-center  rounded mb-3
+//             border-black  border">
+//
+//                <div className="lg:w-[35%] w-[35%]  h-full">
+//
+//                    {/*Add Pop New (buttons)*/}
+//                    <div className="w-full h-1/2 border-r flex justify-center items-center">
+//
+//
+//                        <div className="w-[90%] gap-1 h-[70%] flex justify-center items-center">
+//
+//                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150
+//                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+//
+//                                <Plus className="" />
+//                                Add
+//
+//                            </button>
+//
+//                            <button className="w-[33%] border lg:h-full h-[80%]  rounded cursor-pointer hover:scale-105 duration-150
+//                            active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+//
+//                                <Delete className="" />
+//                                Pop
+//
+//                            </button>
+//
+//                            <button className="w-[33%] border lg:h-full h-[80%] rounded cursor-pointer hover:scale-105 duration-150
+//                             active:scale-100 text-[0px] lg:text-[16px] flex justify-center items-center">
+//
+//                                <Shuffle />
+//                                New
+//
+//                            </button>
+//                        </div>
+//
+//
+//
+//                    </div>
+//
+//                    {/*Animate button*/}
+//                    <div className="w-full h-1/2 border-t border-r flex items-center justify-center">
+//                        <button className="border text-[18px] lg:text-3xl p-1 rounded cursor-pointer hover:scale-105 duration-150
+//                    active:scale-100 flex items-center ">
+//                            <Play />
+//                            Animate
+//                        </button>
+//
+//                    </div>
+//
+//
+//                </div>
+//
+//
+//
+//                {/*Insert and Remove buttons*/}
+//                <div className="lg:w-[60%] w-[55%]  h-full flex flex-col items-center">
+//
+//                    <div className="w-full h-1/2 flex p-1 items-center ">
+//
+//                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
+//                        duration-150 lg:text-[24px] flex items-center justify-center">
+//                            <BetweenVerticalStart />
+//                            Insert
+//                        </button>
+//
+//
+//                        <label htmlFor="value" className="w-[12%] overflow-x-hidden lg:ml-2">Value</label>
+//                        <input type="number" className="w-[22%] ml-1 border-b"
+//                            name="value" />
+//
+//                        <label htmlFor="index" className="w-[12%] overflow-x-hidden lg:ml-2">
+//                            Index
+//                        </label>
+//
+//                        <input type="number" className="w-[22%] ml-1 border-b"
+//                            name="index" min={0} />
+//
+//
+//                    </div>
+//
+//                    <div className="w-full h-1/2 border-t flex p-1 items-center">
+//
+//                        <button className="border text-[0px] p-1 rounded cursor-pointer hover:scale-105
+//                        duration-150 lg:text-[24px] flex items-center justify-center">
+//                            <Scissors />
+//                            Remove
+//                        </button>
+//
+//
+//                        <label htmlFor="index" className="ml-2">index</label>
+//                        <input type="number" className="w-[25%] ml-1 border-b"
+//                            name="index" min={0} />
+//
+//
+//
+//                    </div>
+//                </div>
+//
+//
+//
+//                <div className="lg:w-[5%] w-[10%] border-l h-full flex items-center justify-center cursor-pointer">
+//                    <ArrowRight />
+//                </div>
+//
+//            </div>
 
 
