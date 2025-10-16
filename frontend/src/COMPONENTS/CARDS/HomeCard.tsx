1989fab8039b5c21bfa986be29ad3ef9a762523b
shann <shannrhey123@gmail.com>
2025-10-16 23:47:47 +0800

    shoe

--- frontend/src/COMPONENTS/CARDS/HomeCard.tsx
+++ frontend/src/COMPONENTS/CARDS/HomeCard.tsx
@@ -4,8 +4,8 @@ import algoBG2 from "../../../img/heroBG2.jpg";
 import algoBG3 from "../../../img/heroBG3.jpg";
 
 export default function HomeCard() {
-    return <div className="w-full lg:h-[500px] flex items-center gap-5  lg:p-10 bg-cover bg-center bg-no-repeat 
-    overflow-hidden " style={{
+    return <div className="w-full lg:h-[500px] h-[600px] flex lg:items-center gap-5  lg:p-10 bg-cover bg-center bg-no-repeat 
+    overflow-hidden  items-center justify-center" style={{
             //backgroundImage: `url(${algoBG2})`
 
         }}>
@@ -14,13 +14,16 @@ export default function HomeCard() {
 
 
 
-        <section className="lg:w-[65%] lg:h-[45%]  flex flex-col justify-between  ">
-            <motion.h1 className="text-5xl text-black" initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
-                Understand Algorithms Visually
+        <section className="lg:w-[65%] lg:h-[45%] w-[95%]  flex flex-col justify-between  ">
+            <motion.h1 className="lg:text-5xl text-black text-4xl"
+                initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }}>
+
+                Understand <span className="text-green-400">Algorithms</span> Visually
+
             </motion.h1>
 
             <div className="lg:h-[60%] border-b w-full flex  dark:border-black ">
-                <motion.h1 className="text-[20px] text-black" initial={{ opacity: 0, x: -30 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8 }}>
+                <motion.h1 className="lg:text-[20px] text-black" initial={{ opacity: 0, x: -30 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8 }}>
                     Dive into interactive visualizations of pathfinding, search, and sorting algorithms.
                     Learn how they work — not by reading code, but by watching logic unfold.
                 </motion.h1>
