a6c8fe4f2df76903a1897cdae0525aef0f2a60aa
shann <shannrhey123@gmail.com>
2025-10-25 02:38:16 +0800

    fix 9 frontend dep

--- frontend/src/LIB/axios.ts
+++ frontend/src/LIB/axios.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 
-const baseURL = 'https://algorithm-visualizer-emzo.vercel.app';
+//const baseURL = 'https://algorithm-visualizer-emzo.vercel.app';
 export const AxiosInstance = axios.create({
-    baseURL: baseURL ? baseURL : "http://localhost:5000/algorithm/db/",
+    //baseURL: baseURL ? baseURL : "http://localhost:5000/algorithm/db/",
     //baseURL: `${baseURL}/algorithm/db/`,
+    baseURL: "http://localhost:5000/algorithm/db/",
     withCredentials: true
 
 });
@@ -13,8 +14,9 @@ export const AxiosInstance = axios.create({
 
 
 export const AxiosInstanceAdmin = axios.create({
-    baseURL: baseURL ? baseURL : "http://localhost:5000/secret/admin",
+    //baseURL: baseURL ? baseURL : "http://localhost:5000/secret/admin",
     //baseURL: `${baseURL}/secret/admin`,
+    baseURL: "http://localhost:5000/secret/admin",
     withCredentials: true
 })
 
