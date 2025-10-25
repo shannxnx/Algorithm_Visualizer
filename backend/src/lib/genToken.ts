28942df9feaa36f6d57ccc7470d347c821fd95cf
shann <shannrhey123@gmail.com>
2025-10-26 04:00:43 +0800

    same site = none

--- backend/src/lib/genToken.ts
+++ backend/src/lib/genToken.ts
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import { Response } from "express"
 import { ObjectId } from "mongoose";
 
+
 export const generateToken = (userId: any, res: Response) => {
 
     const token = jwt.sign({ userId }, process.env.JWT_SECRET!, { expiresIn: "1d" });
@@ -9,7 +10,7 @@ export const generateToken = (userId: any, res: Response) => {
     res.cookie("jwt", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: "none",
         secure: process.env.NODE_ENV !== "development"
     });
 
@@ -17,5 +18,7 @@ export const generateToken = (userId: any, res: Response) => {
     return token;
 
 }
+
+
 
 
